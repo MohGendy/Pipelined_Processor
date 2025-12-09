fda99643844e9acc179646036bbb9df172050e2b
Haneen767 <hsalm2421@gmail.com>
2025-12-09 10:28:26 +0200

    Add files via upload

---
+++ PC/2to1_mux.v
@@ -0,0 +1,13 @@
+module mux_2to1 (
+    input  sel,
+    input  [7:0] in0,
+    input  [7:0] in1,
+    output reg [7:0] out
+);
+always @(*) begin
+    if (sel)
+        out = in1;
+    else
+        out = in0;
+end
+endmodule 
