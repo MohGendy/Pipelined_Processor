d28de16dcc35cbd1795c16e007eb203cba2eee5f
Haneen767 <hsalm2421@gmail.com>
2025-12-10 19:48:07 +0200

    Implement reset in instruction register module
    Added reset functionality to instruction register.

--- Instruction_reg/Instruction_reg.v
+++ Instruction_reg/Instruction_reg.v
@@ -1,10 +1,13 @@
 module instruction_reg  (
-    input  clk,
+    input  clk,rst,
     input  [7:0] ir_new,
     output reg [7:0] ir
 );
     always @(posedge clk) begin
-            ir <= ir_new;
+        if (rst)
+            ir <= 0;
+        else
+            ir<= ir_new;
     end
- 
+    
 endmodule
