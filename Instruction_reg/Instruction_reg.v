28e8234fe7907acab2541dd69056b84396f75a9e
Haneen767 <hsalm2421@gmail.com>
2025-12-22 23:40:33 +0200

    Update Instruction_reg.v

--- Instruction_reg/Instruction_reg.v
+++ Instruction_reg/Instruction_reg.v
@@ -3,11 +3,12 @@ module instruction_reg  (
     input  [7:0] ir_new,
     output reg [7:0] ir
 );
-    always @(posedge clk) begin
-        if (rst)
+    always @(posedge clk or negedge rst) begin
+        if (!rst)
             ir <= 0;
         else
             ir<= ir_new;
     end
     
 endmodule
+
