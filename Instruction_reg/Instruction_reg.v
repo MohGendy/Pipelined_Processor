a17642b16cf290a8a787ee06090c1e3db26d4e78
Haneen767 <hsalm2421@gmail.com>
2025-12-23 15:39:38 +0200

    add flush signal

--- Instruction_reg/Instruction_reg.v
+++ Instruction_reg/Instruction_reg.v
@@ -1,10 +1,10 @@
 module instruction_reg  (
-    input  clk,rst,
+    input  clk,rst, flush,
     input  [7:0] ir_new,
     output reg [7:0] ir
 );
     always @(posedge clk or negedge rst) begin
-        if (!rst)
+        if (!rst || flush)
             ir <= 0;
         else
             ir<= ir_new;
